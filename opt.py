f7f81a7e8c412ddfff7c8f196cd6b2f787407c40
seungjooshin <simpsonjoo@postech.ac.kr>
2023-01-08 19:11:37 +0000

    update

--- opt.py
+++ opt.py
@@ -63,7 +63,7 @@ def get_args_parser():
 
     # dataset parameters
     parser.add_argument('--dataset_file', default='coco')
-    parser.add_argument('--coco_path', default='./VOC2007', type=str)
+    parser.add_argument('--coco_path', default='./VOC2COCO', type=str)
     parser.add_argument('--coco_panoptic_path', type=str)
     parser.add_argument('--remove_difficult', action='store_true')
 
