c11f0dde120a508a0a07404d0eb6faf359f8c7b3
seungjooshin <simpsonjoo@postech.ac.kr>
2023-01-08 07:58:30 +0000

    update DETR

--- utils.py
+++ utils.py
@@ -10,6 +10,14 @@ import torch.distributed as dist
 
 import errno
 import os
+import matplotlib.pyplot as plt
+import math
+from PIL import Image
+import requests
+
+from IPython.display import display, clear_output
+import ipywidgets as widgets
+
 
 
 class SmoothedValue(object):
@@ -310,3 +318,155 @@ def init_distributed_mode(args):
     setup_for_distributed(args.rank == 0)
     
     
+class AttentionVisualizer:
+    def __init__(self, model, transform, init_url):
+        self.model = model
+        self.transform = transform
+        self.init_url = init_url
+
+        self.url = ""
+        self.cur_url = None
+        self.pil_img = None
+        self.tensor_img = None
+
+        self.conv_features = None
+        self.enc_attn_weights = None
+        self.dec_attn_weights = None
+
+        self.setup_widgets()
+
+    def setup_widgets(self):
+        self.sliders = [
+            widgets.Text(
+                value=self.init_url,
+                placeholder='Type something',
+                description='URL (ENTER):',
+                disabled=False,
+                continuous_update=False,
+                layout=widgets.Layout(width='100%')
+            ),
+            widgets.FloatSlider(min=0, max=0.99,
+                        step=0.02, description='X coordinate', value=0.72,
+                        continuous_update=False,
+                        layout=widgets.Layout(width='50%')
+                        ),
+            widgets.FloatSlider(min=0, max=0.99,
+                        step=0.02, description='Y coordinate', value=0.40,
+                        continuous_update=False,
+                        layout=widgets.Layout(width='50%')),
+            widgets.Checkbox(
+              value=False,
+              description='Direction of self attention',
+              disabled=False,
+              indent=False,
+              layout=widgets.Layout(width='50%'),
+          ),
+            widgets.Checkbox(
+              value=True,
+              description='Show red dot in attention',
+              disabled=False,
+              indent=False,
+              layout=widgets.Layout(width='50%'),
+          )
+        ]
+        self.o = widgets.Output()
+
+    def compute_features(self, img):
+        model = self.model
+        # use lists to store the outputs via up-values
+        conv_features, enc_attn_weights, dec_attn_weights = [], [], []
+
+        hooks = [
+            model.backbone[-2].register_forward_hook(
+                lambda self, input, output: conv_features.append(output)
+            ),
+            model.transformer.encoder.layers[-1].self_attn.register_forward_hook(
+                lambda self, input, output: enc_attn_weights.append(output[1])
+            ),
+            model.transformer.decoder.layers[-1].multihead_attn.register_forward_hook(
+                lambda self, input, output: dec_attn_weights.append(output[1])
+            ),
+        ]
+        # propagate through the model
+        outputs = model(img)
+
+        for hook in hooks:
+            hook.remove()
+
+        # don't need the list anymore
+        self.conv_features = conv_features[0]
+        self.dec_attn_weights = dec_attn_weights[0]
+        # get the HxW shape of the feature maps of the CNN
+        shape = self.conv_features['0'].tensors.shape[-2:]
+        # and reshape the self-attention to a more interpretable shape
+        self.enc_attn_weights = enc_attn_weights[0].reshape(shape + shape)
+    
+    def compute_on_image(self, url):
+        if url != self.url:
+            self.url = url
+            self.pil_img = Image.open(requests.get(url, stream=True).raw)
+            # mean-std normalize the input image (batch-size: 1)
+            self.tensor_img = self.transform(self.pil_img).unsqueeze(0)
+            self.compute_features(self.tensor_img)
+    
+    def update_chart(self, change):
+        with self.o:
+            clear_output()
+
+            # j and i are the x and y coordinates of where to look at
+            # sattn_dir is which direction to consider in the self-attention matrix
+            # sattn_dot displays a red dot or not in the self-attention map
+            url, j, i, sattn_dir, sattn_dot = [s.value for s in self.sliders]
+
+            fig, axs = plt.subplots(ncols=2, nrows=1, figsize=(9, 4))
+            self.compute_on_image(url)
+
+            # convert reference point to absolute coordinates
+            j = int(j * self.tensor_img.shape[-1])
+            i = int(i * self.tensor_img.shape[-2])
+
+            # how much was the original image upsampled before feeding it to the model
+            scale = self.pil_img.height / self.tensor_img.shape[-2]
+
+            # compute the downsampling factor for the model
+            # it should be 32 for standard DETR and 16 for DC5
+            sattn = self.enc_attn_weights
+            fact = 2 ** round(math.log2(self.tensor_img.shape[-1] / sattn.shape[-1]))
+
+            # round the position at the downsampling factor
+            x = ((j // fact) + 0.5) * fact
+            y = ((i // fact) + 0.5) * fact
+
+            axs[0].imshow(self.pil_img)
+            axs[0].axis('off')
+            axs[0].add_patch(plt.Circle((x * scale, y * scale), fact // 2, color='r'))
+
+            idx = (i // fact, j // fact)
+            
+            if sattn_dir:
+                sattn_map = sattn[idx[0], idx[1], ...]
+            else:
+                sattn_map = sattn[..., idx[0], idx[1]]
+            
+            axs[1].imshow(sattn_map, cmap='cividis', interpolation='nearest')
+            if sattn_dot:
+                axs[1].add_patch(plt.Circle((idx[1],idx[0]), 1, color='r'))
+            axs[1].axis('off')
+            axs[1].set_title(f'self-attention{(i, j)}')
+            
+            fig.tight_layout()
+
+            plt.show()
+
+    def run(self):
+        for s in self.sliders:
+            s.observe(self.update_chart, 'value')
+        self.update_chart(None)
+        url, x, y, d, sattn_d = self.sliders
+        res = widgets.VBox([
+            url,
+            widgets.HBox([x, y]),
+            widgets.HBox([d, sattn_d]),
+            self.o
+        ])
+        return res
